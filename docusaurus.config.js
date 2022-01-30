dd1399a8babefdb79fa9bbdc8ff762a3ebe2a186
Léo Pradel <pradel.leo@gmail.com>
2022-01-30 17:46:16 +0100

    add TODO

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,6 +9,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // TODO migrate content from Gitbook
 // TODO design main page
 // TODO setup CI / CD
+// TODO deploy
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
