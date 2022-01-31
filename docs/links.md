45876dbe000cba51604d63322b3afbd7adcac25a
Léo Pradel <pradel.leo@gmail.com>
2022-01-31 10:09:00 +0100

    feat: first version with docusaurus (#1)

---
+++ docs/links.md
@@ -0,0 +1,27 @@
+---
+sidebar_position: 4
+---
+
+# Official links
+
+- [Application](https://app.sigle.io/)
+- [Landing page](https://www.sigle.io/)
+- [Blog](https://app.sigle.io/sigleapp.id.blockstack/)
+- [Feedback](https://sigle.canny.io/feature-requests/)
+
+## Social
+
+- Follow us on Twitter [@sigleapp](https://twitter.com/sigleapp)
+- Join the [Discord](https://discord.gg/td7WmgK22k)
+- Source code available on [GitHub](https://github.com/sigle)
+
+## The Explorer Guild
+
+3 000 NFT unique and randomly generated NFTs on the Stacks blockchain. Hand drawn and inspired by the famous writer Jules Verne.
+
+- [Landing page](https://www.explorerguild.io/)
+- [Buy on STXNFT](https://stxnft.com/collections/SP2X0TZ59D5SZ8ACQ6YMCHHNR2ZN51Z32E2CJ173.the-explorer-guild/)
+
+## Other projects
+
+- [StxStats](https://www.stxstats.co/): Get the latest data from Stacks blockchain.
---
+++ docusaurus.config.js
@@ -0,0 +1,118 @@
+// @ts-check
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// TODO setup fathom
+// TODO migrate content from Gitbook
+// TODO meta SEO
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Sigle',
+  url: 'https://docs.sigle.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'sigle',
+  projectName: 'docs',
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/github/sigle/tree/main/',
+        },
+        theme: {
+          customCss: [
+            require.resolve('./src/css/custom.css'),
+            require.resolve('@fontsource/open-sans'),
+          ],
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Sigle Docs',
+        logo: {
+          alt: 'Sigle Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            href: 'https://github.com/pradel/sigle',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Connect',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/sigleapp',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/td7WmgK22k',
+              },
+              {
+                label: 'Github',
+                href: 'https://github.com/sigle',
+              },
+            ],
+          },
+          {
+            title: 'Product',
+            items: [
+              {
+                label: 'Application',
+                href: 'https://app.sigle.io',
+              },
+              {
+                label: 'Blog',
+                href: 'https://app.sigle.io/sigleapp.id.blockstack',
+              },
+              {
+                label: 'Feedback',
+                href: 'https://sigle.canny.io/feature-requests',
+              },
+            ],
+          },
+          {
+            title: 'More...',
+            items: [
+              {
+                label: 'The Explorer Guild',
+                href: 'https://www.explorerguild.io/',
+              },
+              {
+                label: 'StxStats',
+                href: 'https://www.stxstats.co/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Sigle`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
